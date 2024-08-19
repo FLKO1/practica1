68d036fc0be070b7a9ab9ccce0f49a9ac4ec38c4
flk0ng <fco.lazokong.work@gmail.com>
2024-08-19 11:49:45 -0400

    Revert "Estado, agregado text y update"

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,23 +1,12 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function Component(){
-
-    const   [text, setText] = useState()
-    const   [update, setUpdate]= useState()
-
-     const textOnChange = (event)=> {
-        setText(event.target.value)
-     }
-
-     const buttonOnClick = ()=>{
-        setUpdate(text)
-     }
     return(
         <div>
-            <input type="text" value={text} onChange={textOnChange} />
-            <button onClick={buttonOnClick}>Guardar</button>
-            <p>Texto Input: {text}</p>
-            <p>Texto Actualizado: {update}</p>
+            <input type="text" />
+            <button>Guardar</button>
+            <p>Texto Input</p>
+            <p>Texto Actualizado</p>
         </div>
     )
 }
