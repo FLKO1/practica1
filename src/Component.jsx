8f44b019b0b3c923cdc2781d733077b450586639
flk0ng <fco.lazokong.work@gmail.com>
2024-08-19 11:25:25 -0400

    Estado, agregado text y update

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Component(){
+
+    const   [text, setText] = useState()
+    const   [update, setUpdate]= useState()
+
+     const textOnChange = (event)=> {
+        setText(event.target.value)
+     }
+
+     const buttonOnClick = ()=>{
+        setUpdate(text)
+     }
     return(
         <div>
-            <input type="text" />
-            <button>Guardar</button>
-            <p>Texto Input</p>
-            <p>Texto Actualizado</p>
+            <input type="text" value={text} onChange={textOnChange} />
+            <button onClick={buttonOnClick}>Guardar</button>
+            <p>Texto Input: {text}</p>
+            <p>Texto Actualizado: {update}</p>
         </div>
     )
 }
