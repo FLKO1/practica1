f2d9da5a4a64291a9817d4eef47da7b7c49ce4ba
FLKO1 <83712998+FLKO1@users.noreply.github.com>
2024-08-19 01:26:38 -0400

    Botones/Componentes

---
+++ src/Component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Component(){
+    return(
+        <div>
+            <input type="text" />
+            <button>Guardar</button>
+            <p>Texto Input</p>
+            <p>Texto Actualizado</p>
+        </div>
+    )
+}
